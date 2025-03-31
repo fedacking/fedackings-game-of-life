3a619e61042251985a5580c3205d10c6c9f2881b
Francisco Xavier Gauna <francisco.gauna@lambdaclass.com>
2025-03-31 15:24:10 -0300

    Added test
    Added multiple tests checking the basic rules of the Game of Life

--- src/gol.rs
+++ src/gol.rs
@@ -1,39 +1,36 @@
-use std::{cell::Cell, ops::{Index, IndexMut}};
+use std::ops::{Index, IndexMut};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum CellState {
-    Alive, Dead
+    Alive,
+    Dead,
 }
 
 impl CellState {
     fn pretty_print(self) -> char {
         match self {
             CellState::Alive => 'A',
-            CellState::Dead => 'D'
+            CellState::Dead => 'D',
         }
     }
 }
 
 fn update_cell(state: CellState, neighbours: usize) -> CellState {
     match state {
-        CellState::Alive => {
-            match neighbours {
-                2..3 => CellState::Alive,
-                _ => CellState::Dead
-            }
+        CellState::Alive => match neighbours {
+            2..3 => CellState::Alive,
+            _ => CellState::Dead,
         },
-        CellState::Dead => {
-            match neighbours {
-                3 => CellState::Alive,
-                _ => CellState::Dead
-            }
-        }
+        CellState::Dead => match neighbours {
+            3 => CellState::Alive,
+            _ => CellState::Dead,
+        },
     }
 }
 
 #[derive(Debug, Clone, Copy)]
-pub struct GameOfLife<const WIDTH: usize, const HEIGHT: usize> { 
-    board: [[CellState; WIDTH]; HEIGHT]
+pub struct GameOfLife<const WIDTH: usize, const HEIGHT: usize> {
+    board: [[CellState; WIDTH]; HEIGHT],
 }
 
 impl<const WIDTH: usize, const HEIGHT: usize> Index<(usize, usize)> for GameOfLife<WIDTH, HEIGHT> {
@@ -43,19 +40,26 @@ impl<const WIDTH: usize, const HEIGHT: usize> Index<(usize, usize)> for GameOfLi
         let (x, y) = index;
         &self.board[y][x]
     }
-}   
+}
 
-impl<const WIDTH: usize, const HEIGHT: usize> IndexMut<(usize, usize)> for GameOfLife<WIDTH, HEIGHT> {
+impl<const WIDTH: usize, const HEIGHT: usize> IndexMut<(usize, usize)>
+    for GameOfLife<WIDTH, HEIGHT>
+{
     fn index_mut(&mut self, index: (usize, usize)) -> &mut CellState {
         let (x, y) = index;
         &mut self.board[y][x]
     }
-}   
-
+}
 
 impl<const WIDTH: usize, const HEIGHT: usize> GameOfLife<WIDTH, HEIGHT> {
     pub fn new() -> Self {
-        GameOfLife { board: [[CellState::Dead; WIDTH]; HEIGHT] }
+        GameOfLife {
+            board: [[CellState::Dead; WIDTH]; HEIGHT],
+        }
+    }
+
+    pub fn from_board(board: [[CellState; WIDTH]; HEIGHT]) -> Self {
+        GameOfLife { board }
     }
 
     pub fn pretty_print(&self) {
@@ -64,41 +68,109 @@ impl<const WIDTH: usize, const HEIGHT: usize> GameOfLife<WIDTH, HEIGHT> {
         }
     }
 
-    fn y_permut(&self,  x: usize, y: usize) -> Vec<(usize, usize)> {
+    fn y_permut(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
         let mut indexes: Vec<(usize, usize)> = vec![(x, y)];
-        if y > 0 { indexes.push((x, y - 1));}
-        if y < (HEIGHT - 1) { indexes.push((x, y + 1));}
+        if y > 0 {
+            indexes.push((x, y - 1));
+        }
+        if y < (HEIGHT - 1) {
+            indexes.push((x, y + 1));
+        }
         indexes
     }
 
     pub fn count_neighbours(&self, x: usize, y: usize) -> usize {
         let mut indexes: Vec<(usize, usize)> = vec![];
 
-        if  x > 0 { indexes.extend(self.y_permut(x - 1, y)); }
-        if  x < (WIDTH - 1) { indexes.extend(self.y_permut(x + 1, y)); }
+        if x > 0 {
+            indexes.extend(self.y_permut(x - 1, y));
+        }
+        if x < (WIDTH - 1) {
+            indexes.extend(self.y_permut(x + 1, y));
+        }
         indexes.extend(self.y_permut(x, y));
         indexes.retain(|index| *index != (x, y));
 
-        indexes.iter().filter(|cell| matches!(self[**cell], CellState::Alive)).count()
-    }
-
-    pub fn count_neighbours_all(&self) -> [[usize; WIDTH]; HEIGHT]{
-        let mut result = [[0; WIDTH]; HEIGHT];
-        for y in 0..HEIGHT {
-            for x in 0..WIDTH {
-                result[y][x] = self.count_neighbours(x, y);
-            }
-        }
-        result
+        indexes
+            .iter()
+            .filter(|cell| matches!(self[**cell], CellState::Alive))
+            .count()
     }
 
     pub fn update(&mut self) {
         let mut new_board = [[CellState::Dead; WIDTH]; HEIGHT];
-        for y in 0..HEIGHT {
-            for x in 0..WIDTH {
-                new_board[y][x] = update_cell(self[(x, y)], self.count_neighbours(x, y))
+        for (y, column) in self.board.iter().enumerate() {
+            for (x, state) in column.iter().enumerate() {
+                new_board[y][x] = update_cell(*state, self.count_neighbours(x, y))
             }
         }
         self.board = new_board;
+    }
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize> PartialEq for GameOfLife<WIDTH, HEIGHT> {
+    fn eq(&self, other: &Self) -> bool {
+        for (y, column) in self.board.iter().enumerate() {
+            for (x, element) in column.iter().enumerate() {
+                if *element != other[(x, y)] {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const D: CellState = CellState::Dead;
+    const A: CellState = CellState::Alive;
+
+    #[test]
+    fn test_empty() {
+        let default_gol: GameOfLife<6, 6> = GameOfLife::new();
+        let mut update_gol: GameOfLife<6, 6> = GameOfLife::new();
+        update_gol.update();
+
+        assert_eq!(default_gol, update_gol);
+    }
+
+    #[test]
+    fn test_overcrowding() {
+        let default_gol: GameOfLife<6, 6> = GameOfLife::new();
+        let start_gol: GameOfLife<6, 6> = GameOfLife {
+            board: [
+                [A, A, A, A, A, A],
+                [A, A, A, A, A, A],
+                [A, A, A, A, A, A],
+                [A, A, A, A, A, A],
+                [A, A, A, A, A, A],
+                [A, A, A, A, A, A],
+            ],
+        };
+        let mut update_gol: GameOfLife<6, 6> = start_gol;
+        update_gol.update();
+
+        assert_eq!(default_gol, update_gol);
+        assert_ne!(start_gol, update_gol);
+    }
+
+    #[test]
+    fn test_repeat() {
+        let a_gol: GameOfLife<3, 3> = GameOfLife {
+            board: [[D, A, D], [D, A, D], [D, A, D]],
+        };
+        let b_gol: GameOfLife<3, 3> = GameOfLife::from_board([[D, D, D], [A, A, A], [D, D, D]]);
+        let mut update_gol: GameOfLife<3, 3> = a_gol;
+
+        assert_eq!(a_gol, update_gol);
+        update_gol.update();
+        assert_eq!(b_gol, update_gol);
+        update_gol.update();
+        assert_eq!(a_gol, update_gol);
+        update_gol.update();
+        assert_eq!(b_gol, update_gol);
     }
 }
