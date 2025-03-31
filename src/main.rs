524a66f1af17e66fbcb04c3182c85199f88837f1
Francisco Xavier Gauna <francisco.gauna@lambdaclass.com>
2025-03-31 14:39:18 -0300

    Initial Logic commit
    Added the basic logic of the game of life, with a
    structure for the data, and an update function.
    All of that is present in the gol.rs file, the
    main file just has some basic test code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod gol;
+use gol::{GameOfLife, CellState};
+
 fn main() {
-    println!("Hello, world!");
+    let mut gol: GameOfLife<8, 8> = GameOfLife::new();
+    gol[(3, 3)] = CellState::Alive;
+    gol.pretty_print();
+    println!("{:?}", gol.count_neighbours_all());
+    gol.update();
+    gol.pretty_print();
 }
